9ff8c0e15018ff88ee55a8347c8f4fd0f74907f9
PrimusLV <k.drivnieks@inbox.lv>
2016-08-29 15:46:19 +0300

    Better error reporting.

--- src/legionpe/theta/utils/ReportErrorTask.php
+++ src/legionpe/theta/utils/ReportErrorTask.php
@@ -38,12 +38,14 @@ class ReportErrorTask extends AsyncTask{
 		if(microtime(true) - self::$last0 < 60){
 			return;
 		}
-		self::log("ping-all !!! PEMapModder ping!");
-		self::log("Exception caught: " . $this->exMsg);
-		self::log("In file: " . $this->exFile . "#" . $this->exLine);
-		self::log("Happened during: " . $this->when);
-		self::log("On the server below:");
-		Utils::getURL(Credentials::IRC_WEBHOOK_NOPREFIX . urlencode("BotsHateNames: status " . Settings::$LOCALIZE_IP . " " . Settings::$LOCALIZE_PORT));
+		$message = "=== ERROR BEGIN ===".PHP_EOL;
+		$message .= "Exception: " . $this->exMsg.PHP_EOL;
+		$message .= "File: " . $this->exFile . "#" . $this->exLine.PHP_EOL;
+		$message .= "UNIX Timestamp: " . $this->when.PHP_EOL;
+		$message .= "Time: ".date("H:i:s", $this->when).PHP_EOL;
+		$message .= "Server: ".Settings::$LOCALIZE_IP . " " . Settings::$LOCALIZE_PORT." (".Settings::$LOCALIZE_ID.")".PHP_EOL;
+		$message .= "=== END ERROR ===".PHP_EOL;
+		self::log($message);
 		self::$last0 = self::$last1;
 		self::$last1 = self::$last2;
 		self::$last2 = self::$last3;
@@ -51,6 +53,7 @@ class ReportErrorTask extends AsyncTask{
 	}
 	public static function log($line){
 		echo $line, PHP_EOL;
-		Utils::getURL(Credentials::IRC_WEBHOOK . urlencode($line));
+		Utils::getURL(Credentials::IRC_WEBHOOK_NOPREFIX . urlencode($line));
+		Utils::postURL(Credentials::SLACK_WEBHOOK, ["payload" => json_encode(["text" => $line, "username" => "Server-".Settings::$LOCALIZE_ID.""])]);
 	}
 }
